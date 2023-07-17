4067936ed32a573357fc1979aa79e7211192e807
TimCauss <t.caussignac@gmail.com>
2023-07-17 13:58:53 +0200

    update

--- robotfriends/src/App.js
+++ robotfriends/src/App.js
@@ -1,7 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import React from "react";
 
-function App() {
+class App extends React.Component {
+
+  render() {
+
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +24,7 @@ function App() {
       </header>
     </div>
   );
+  }
 }
 
 export default App;
