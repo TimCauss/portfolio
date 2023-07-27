58a301c769ab941558594e0d445a043dc2f97efd
TimCauss <t.caussignac@gmail.com>
2023-07-27 15:52:59 +0200

    Rename Dashboard nav to DashboardHeader Change calls

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import PageHeader from "../../components/PageHeader";
+import DashboardHeader from "../../components/DashboardHeaderjsx";
 
 export default function Dashboard() {
   return (
     <section className="dashboardTitle">
       <PageHeader title="Dashboard" />
-      <NavDashboard />
+      <DashboardHeader />
     </section>
   );
 }
