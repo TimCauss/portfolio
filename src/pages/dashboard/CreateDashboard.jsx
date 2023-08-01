82af07b7a6d729fac46fef9963033acd854cecca
TimCauss <t.caussignac@gmail.com>
2023-08-01 11:26:50 +0200

    created CreateDashboard CreateDashboard component is used to create new projects

---
+++ src/pages/dashboard/CreateDashboard.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+
+const CreateDashboard = (props) => {
+  return (
+    <>
+      <div className="d-flex flex-column">
+        <input
+          type="text"
+          placeholder="Titre"
+          name="title"
+          value={props.singledata.title}
+          onChange={props.handleChange}
+        />
+
+        <input
+          type="text"
+          placeholder="Image Url"
+          name="image"
+          value={props.singledata.image}
+          onChange={props.handleChange}
+        />
+
+        <input
+          type="text"
+          placeholder="Techno"
+          name="technologies"
+          value={props.singledata.technologies}
+          onChange={props.handleChange}
+        />
+
+        <input
+          type="text"
+          placeholder="description"
+          name="description"
+          value={props.singledata.description}
+          onChange={props.handleChange}
+        />
+
+        <input
+          type="text"
+          placeholder="github"
+          name="github"
+          value={props.singledata.github}
+          onChange={props.handleChange}
+        />
+        <input
+          type="text"
+          placeholder="deployed"
+          name="deployed"
+          value={props.singledata.deployed}
+          onChange={props.handleChange}
+        />
+        <input
+          type="text"
+          placeholder="bgcolor"
+          name="bgcolor"
+          value={props.singledata.bgcolor}
+          onChange={props.handleChange}
+        />
+      </div>
+      <div className="modal-footer">
+        <button type="button" className="btn" onClick={props.createProject}>
+          Create
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default CreateDashboard;
--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import PageHeader from "../../components/PageHeader";
 import DashboardHeader from "../../components/DashboardHeader.jsx";
 import ListDashboard from "../dashboard/ListDashboard";
+import CreateDashboard from "./CreateDashboard";
 
 export default class Dashboard extends React.Component {
   constructor(props) {
@@ -21,27 +22,57 @@ export default class Dashboard extends React.Component {
     };
     this.getProjectsList = this.getProjectsList.bind(this);
     this.createProject = this.createProject.bind(this);
+    this.getList = this.getList.bind(this);
   }
 
   createProject() {
     fetch("http://localhost:3001/Projects", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ name: "myproject" }).then(
-        this.setState({
-          singledata: {
-            title: "",
-            image: "",
-            technologies: "",
-            description: "",
-            github: "",
-            deployed: "",
-            bgcolor: "",
-          },
-        })
-      ),
-    });
+      body: JSON.stringify(this.state.singledata),
+    }).then(
+      this.setState({
+        singledata: {
+          title: "",
+          image: "",
+          technologies: "",
+          description: "",
+          github: "",
+          deployed: "",
+          bgcolor: "",
+        },
+      })
+    );
   }
+
+  handleChange = (e) => {
+    let title = this.state.singledata.title;
+    let image = this.state.singledata.image;
+    let technologies = this.state.singledata.technologies;
+    let description = this.state.singledata.description;
+    let github = this.state.singledata.github;
+    let deployed = this.state.singledata.deployed;
+    let bgcolor = this.state.singledata.bgcolor;
+    if (e.target.name === "title") title = e.target.value;
+    if (e.target.name === "image") image = e.target.value;
+    if (e.target.name === "technologies") technologies = e.target.value;
+    if (e.target.name === "description") description = e.target.value;
+    if (e.target.name === "github") github = e.target.value;
+    if (e.target.name === "deployed") deployed = e.target.value;
+    if (e.target.name === "bgcolor") bgcolor = e.target.value;
+
+    this.setState({
+      singledata: {
+        title: title,
+        image: image,
+        technologies: technologies,
+        description: description,
+        github: github,
+        deployed: deployed,
+        bgcolor: bgcolor,
+      },
+    });
+  };
 
   getProjectsList() {
     this.setState({ loading: true }, () => {
@@ -52,7 +83,52 @@ export default class Dashboard extends React.Component {
     });
   }
 
+  getList(event, id) {
+    this.setState(
+      {
+        singledata: {
+          title: "Loading...",
+          image: "Loading...",
+          technologies: "Loading...",
+          description: "Loading...",
+          github: "Loading...",
+          deployed: "Loading...",
+          bgcolor: "Loading...",
+        },
+      },
+      () => {
+        fetch("https://localhost:3001/Projects" + id)
+          .then((res) => res.json())
+          .then((result) => {
+            this.setState({
+              singledata: {
+                title: result.title,
+                image: result.image ? result.image : "",
+                technologies: result.technologies,
+                description: result.technologies,
+                github: result.github ? result.github : "#",
+                deployed: result.deployed ? result.deployed : "#",
+                bgcolor: result.bgcolor ? result.bgcolor : "#FFF",
+              },
+            });
+          });
+      }
+    );
+  }
+
   render() {
+    const listTable = this.state.loading ? (
+      <span>Loading...</span>
+    ) : (
+      <ListDashboard
+        alldata={this.state.alldata}
+        singledata={this.state.singledata}
+        getList={this.getList}
+        // updateList={this.updateList}
+        // deleteList={this.deleteList}
+        handleChange={this.handleChange}
+      />
+    );
     return (
       <section className="dashboardTitle">
         <PageHeader title="Dashboard" />
@@ -61,9 +137,14 @@ export default class Dashboard extends React.Component {
           <button className="btn mx-4" onClick={this.getProjectsList}>
             Update les données
           </button>
-          <button className="btn mx-4 fa-add"></button>
         </div>
-        <ListDashboard alldata={this.state.alldata} />
+        <CreateDashboard
+          singledata={this.state.singledata}
+          createProject={this.createProject}
+          handleChange={this.handleChange}
+        />
+        <br />
+        {listTable}
       </section>
     );
   }
